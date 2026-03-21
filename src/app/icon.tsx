dc4c201106565884900f88d4a654f90748e9ea8a
YuriDorigon <yuridorigon12@gmail.com>
2026-03-21 16:15:17 -0300

    ASDASD

---
+++ src/app/icon.tsx
@@ -0,0 +1,3 @@
+// src/app/icon.tsx
+// Este ficheiro foi removido para melhorar a performance e estabilidade do servidor de desenvolvimento.
+// O Next.js irá procurar por um ficheiro estático /public/favicon.ico.
