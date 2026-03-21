b7ff895c04b948e73283cdfefee8a6d6fded9ef3
YuriDorigon <yuridorigon12@gmail.com>
2026-03-21 16:14:24 -0300

    Update icon.tsx

--- src/app/icon.tsx
+++
@@ -1,3 +0,0 @@
-// src/app/icon.tsx
-// Este ficheiro foi removido para melhorar a performance e estabilidade do servidor de desenvolvimento.
-// O Next.js irá procurar por um ficheiro estático /public/favicon.ico.
